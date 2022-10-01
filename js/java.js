056512b2787ac5a038805bfd6edae5c829f36e31
Jonathan Moreno <johnfrom209@gmail.com>
2022-10-01 16:10:01 -0700

    added content to html and styled them in css. js has psuedo code

---
+++ js/java.js
@@ -0,0 +1,11 @@
+//display a btn
+//on click start quiz and start timer
+//create an array of questions with multiple choice
+    //radio button because checkbox will not be right 
+    //submit at the bottom
+//display question and options to page
+//if user chooses wrong decrease timer
+    //optional: give a shake or maybe a red highlight
+//else move to next question
+//repeat until end of quiz
+//save score
